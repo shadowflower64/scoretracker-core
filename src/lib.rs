c9fc8c4470abd0c01a8ede08cf29d10f7ece00cc
Iris <shadowflower64@proton.me>
2025-09-25 14:42:50 +0200

    Add links to components in docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,20 @@
 //!
 //! # Components
 //! Here is a list of 12 main components that "scoretracker" can be divided into:
-//! | #  | Module name          | Description                                                                          | Progress |
-//! | -- | -------------------- | ------------------------------------------------------------------------------------ | -------- |
-//! | 1  | **Hive**             | Handles various computation tasks and dispatches workers to execute those tasks.     |      20% |
-//! | 2  | **Library**          | A database of rhythm game achievement proofs (mainly screenshots and videos).        |      10% |
-//! | 3  | **YouTubeManager**   | Handles uploading proofs to YouTube and assigns the metadata using the YouTube API.  |     0.1% |
-//! | 4  | **Scoreboard**       | A database of rhythm game plays/scores/performances.                                 |       2% |
-//! | 5  | **SongDB**           | A database of songs from rhythm games, along with their note counts and other stats. |     0.1% |
-//! | 6  | **scoretracker-cli** | Command-Line Interface for all scoretracker features.                                |      15% |
-//! | 7  | **scoretracker-api** | HTTP server with a JSON API interface for most or all scoretracker features.         |       0% |
-//! | 8  | **scoretracker-web** | Web frontend that connects to the scoretracker-api mentioned above.                  |     0.1% |
-//! | 9  | **obs-frontend**     | A web-based frontend designed specifically for OBS, for displaying stats etc.        |       0% |
-//! | 10 | **scoretracker-gui** | Probably Qt-based GUI application with all scoretracker features. (JS is awful)      |       0% |
-//! | 11 | **OCR**              | Automatic proof reading.                                                             |     0.1% |
-//! | 12 | **ReplayReader**     | Game-specific file parsing, mainly for reading replays.                              |       0% |
+//! | #  | Component name               | Description                                                                          | Progress |
+//! | -- | ---------------------------- | ------------------------------------------------------------------------------------ | -------- |
+//! | 1  | [**Hive**](hive)             | Handles various computation tasks and dispatches workers to execute those tasks.     |      20% |
+//! | 2  | [**Library**](library)       | A database of rhythm game achievement proofs (mainly screenshots and videos).        |      10% |
+//! | 3  | **YouTubeManager**           | Handles uploading proofs to YouTube and assigns the metadata using the YouTube API.  |     0.1% |
+//! | 4  | [**Scoreboard**](scoreboard) | A database of rhythm game plays/scores/performances.                                 |       2% |
+//! | 5  | [**SongDB**](songdb)         | A database of songs from rhythm games, along with their note counts and other stats. |     0.1% |
+//! | 6  | **scoretracker-cli**         | Command-Line Interface for all scoretracker features.                                |      15% |
+//! | 7  | **scoretracker-api**         | HTTP server with a JSON API interface for most or all scoretracker features.         |       0% |
+//! | 8  | **scoretracker-web**         | Web frontend that connects to the scoretracker-api mentioned above.                  |     0.1% |
+//! | 9  | **obs-frontend**             | A web-based frontend designed specifically for OBS, for displaying stats etc.        |       0% |
+//! | 10 | **scoretracker-gui**         | Probably Qt-based GUI application with all scoretracker features. (JS is awful)      |       0% |
+//! | 11 | **OCR**                      | Automatic proof reading.                                                             |     0.1% |
+//! | 12 | **ReplayReader**             | Game-specific file parsing, mainly for reading replays.                              |       0% |
 //!
 //! This crate specifically will include components 1..=5 and 11..=12. Components 6..=10 will be separate, but built upon the `scoretracker_core` library.
 //!
